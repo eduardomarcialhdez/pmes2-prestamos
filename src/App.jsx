f8265adaa36267a67ab997122170d271f032b172
Eduardo Marcial Hernández <111074954+eduardomarcialhdez@users.noreply.github.com>
2023-10-27 22:35:39 -0600

    Calculadora de intereses

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,32 @@
 import { useState } from "react";
 
 function App() {
+  const [monto, setMonto] = useState(0);
+  const [plazo, setPlazo] = useState(1);
+
+  const tasaInteresBaja = 0.05; // 5% mensual
+  const tasaInteresAlta = 0.15; // 15% mensual
+  const iva = 0.16; // 16%
+
+  const calcularTotalAPagar = () => {
+    const interesBajo = monto * tasaInteresBaja * plazo;
+    const interesAlto = monto * tasaInteresAlta * plazo;
+    const totalBajo = monto + interesBajo + (monto + interesBajo) * iva;
+    const totalAlto = monto + interesAlto + (monto + interesAlto) * iva;
+
+    return [totalBajo, totalAlto];
+  };
+
+  const handleMontoChange = (event) => {
+    setMonto(parseFloat(event.target.value));
+  };
+
+  const handlePlazoChange = (value) => {
+    setPlazo(value);
+  };
+
+  const [totalBajo, totalAlto] = calcularTotalAPagar();
+
   return (
     <>
       <div>
@@ -83,7 +109,71 @@ function App() {
           </div>
         </div>
       </div>
-      <div></div>
+      <div>
+        <div className="loan-section mt-5">
+          <h2>Calculadora de Préstamos</h2>
+          <div className="input-group mb-3">
+            <input
+              type="number"
+              className="form-control"
+              placeholder="Monto a Prestar"
+              onChange={handleMontoChange}
+            />
+            <div className="input-group-append">
+              <span className="input-group-text">MXN</span>
+            </div>
+          </div>
+          <div className="plazo-buttons">
+            <button
+              className={`btn ${plazo === 1 ? "btn-primary" : "btn-secondary"}`}
+              onClick={() => handlePlazoChange(1)}
+            >
+              1 Mes
+            </button>
+            <button
+              className={`btn ${plazo === 3 ? "btn-primary" : "btn-secondary"}`}
+              onClick={() => handlePlazoChange(3)}
+            >
+              3 Meses
+            </button>
+            <button
+              className={`btn ${plazo === 6 ? "btn-primary" : "btn-secondary"}`}
+              onClick={() => handlePlazoChange(6)}
+            >
+              6 Meses
+            </button>
+            <button
+              className={`btn ${plazo === 9 ? "btn-primary" : "btn-secondary"}`}
+              onClick={() => handlePlazoChange(9)}
+            >
+              9 Meses
+            </button>
+            <button
+              className={`btn ${
+                plazo === 12 ? "btn-primary" : "btn-secondary"
+              }`}
+              onClick={() => handlePlazoChange(12)}
+            >
+              12 Meses
+            </button>
+          </div>
+        </div>
+
+        {/* Sección para mostrar el total a pagar con tasas de interés e IVA */}
+        <div className="total-section mt-5">
+          <h2>Total a Pagar</h2>
+          <div className="row">
+            <div className="col-md-6">
+              <h3>Interés al 5%</h3>
+              <p>Total a pagar (con IVA): ${totalBajo.toFixed(2)}</p>
+            </div>
+            <div className="col-md-6">
+              <h3>Interés al 15%</h3>
+              <p>Total a pagar (con IVA): ${totalAlto.toFixed(2)}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
